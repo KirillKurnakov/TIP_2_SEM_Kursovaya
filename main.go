05b424487430f1bcc6398c5b3d46cb5f41b47311
Kirill <kirilkanote@gmail.com>
2025-05-24 22:26:44 +0300

    Final kursovaya 2

--- main.go
+++ main.go
@@ -774,7 +774,7 @@ func main() {
 	initDB()
 	time.Sleep(10 * time.Second)
 	var router = gin.Default()
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, ginSwagger.URL("http://45.90.35.111/swagger/doc.json")))
 	protected := router.Group("/")
 
 	router.POST("/login", login)
